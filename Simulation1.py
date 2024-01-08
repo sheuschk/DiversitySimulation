58d29af96af0707c8be98958be368d58fbd60e06
Simon Heuschkel <simon.heuschkel@icloud.com>
2024-01-08 15:17:57 +0100

    Simulation 1 Script fixes and visualization

---
+++ Simulation1.py
@@ -0,0 +1,315 @@
+import argparse
+import sys
+import os
+import json
+import numpy as np
+
+from environments import Environment2D, NK_landscape_loaded
+from datetime import datetime
+
+from scipy.special import softmax
+
+import pickle
+
+from helpers import run_intervention, loss_gradients, clip_steps, draw_step, bit_switch, parse_NK
+
+
+def local_search_2D(env: np.array, agent_value, eta: float, position: np.array, steps, tau, rng):
+    """Stochastic Hill climbing OR random step (only if better)"""
+    if eta >= rng.random():
+        rand_pos = rng.integers(0, env.grid_size, 2)
+        rand_value = env.get_fitness(rand_pos)
+        if rand_value > agent_value:
+            return rand_pos.reshape(1, 2), np.array([1.])
+    neighbour_states = [[clip_steps(position[0]+s[0], env.grid_size), clip_steps(position[1]+s[1], env.grid_size)] for s in steps]
+    ind_values = np.array([env.get_fitness(p) for p in neighbour_states])
+    ind_values_pb = softmax((ind_values / tau))
+    return np.asarray(neighbour_states), ind_values_pb
+
+
+def local_search_NK(env, state, eta):        
+    digits = rng.choice(20, size=(5), replace=False)
+    neigh_states = np.zeros((len(digits) +1, env.N)).astype(int)
+    neigh_states[0] = state 
+    neigh_states[1:] = np.array([bit_switch(state.copy(), digit) for digit in digits])
+    neigh_scores = np.array([env.get_fitness(ns) for ns in neigh_states])
+    neigh_pb = softmax((neigh_scores * eta))
+    return neigh_states, neigh_pb
+
+
+def local_search(env_type, env, S, eta, solution, all_steps, tau, rng):
+    if env_type == "NK":
+        new_solutions, probability_distribution = local_search_NK(env, solution, eta)
+    elif env_type == "2D":
+        new_solutions, probability_distribution = local_search_2D(env, S, eta, solution, all_steps, tau, rng)
+    return new_solutions, probability_distribution
+
+
+def create_roleModel_features(n_roleModels, rng=None):
+    """Feature matrix F (n_roleModels x features): F[agent_i] = [gamma, eta, n0, n1, ...]"""
+    gammas = np.clip(rng.normal(0.8, 0.05, size=n_roleModels), 0, 1)
+    etas = np.clip(rng.normal(0.8, 0.05, size=n_roleModels), 0, 1)
+    noise = rng.random(n_roleModels)
+    rho = rng.random(n_roleModels) * 0.2
+    F = np.stack((gammas, etas, noise, rho), axis=1)
+    return F
+
+
+def initialize_role_models(env_type, env, n_roleModels, rng):
+    """Init less privileged agents, but explicitly good performance and traits"""
+    F = create_roleModel_features(n_roleModels, rng)
+    
+    if env_type == "NK":
+        fitness_values = env.get_all_values()
+        max_values = np.sort(fitness_values)[-100:]
+        S = rng.choice(max_values, size=n_roleModels)
+        return F, S
+    elif env_type == "2D":
+        fitness_values = env.get_all_values()
+        quantile = np.quantile(fitness_values, 0.9)
+        high_pos = np.where((np.round(quantile, 3) < np.round(fitness_values, 3)))
+        start_pos_index = rng.integers(0, len(high_pos[0]), n_roleModels)
+        all_positions = np.stack((high_pos[0][start_pos_index], high_pos[1][start_pos_index]), axis=1)
+        S = fitness_values[all_positions[:, 0], all_positions[:, 1]]
+        return F, S
+
+def initialize_starting_position_NK(env, n_agents, rho):
+    fitness_values = env.get_all_values()
+    
+    quantile = np.quantile(fitness_values, rho)
+    minority_index = rho <= 0.2
+    
+    S = np.zeros(n_agents)
+    states = np.empty((n_agents, N)).astype(int)
+    
+    for _, q in enumerate(quantile):
+        if minority_index[_] == True:
+            positions = np.where((np.round(q, 2) >= np.round(fitness_values, 2)))[0]
+        else:
+            positions = np.where((np.round(q, 2) <= np.round(fitness_values, 2)))[0]
+            
+        index = rng.choice(positions).astype(int)
+        S[_] = fitness_values[index]
+        states[_] = np.array(list(f'{index:020b}')).astype(int)
+    return states, S
+
+def initialize_starting_position_2D(env, n_agents, rho):
+    minority_index = rho <= 0.2
+    fitness_values = env.get_all_values()
+    quantile = np.quantile(fitness_values, rho)
+
+    solutions = np.empty((n_agents, 2), dtype=int)
+
+    for _, q in enumerate(quantile):
+        if minority_index[_] == True:
+            q_solutions = np.where((np.round(q, 2) >= np.round(fitness_values, 2)))
+            index = rng.integers(0, len(q_solutions[0]))
+            solutions[_] = [q_solutions[0][index], q_solutions[1][index]]
+        else:
+            q_solutions = np.where((np.round(q, 2) <= np.round(fitness_values, 2)))
+            index = rng.integers(0, len(q_solutions[0]))
+            solutions[_] = [q_solutions[0][index], q_solutions[1][index]]
+    
+    S = fitness_values[solutions[:, 0], solutions[:, 1]]
+    return solutions, S
+
+def initialize_starting_position(env_type, env, n_agents, rho):
+    if env_type == "NK":
+        return initialize_starting_position_NK(env, n_agents, rho)
+    elif env_type == "2D":
+        return initialize_starting_position_2D(env, n_agents, rho)
+
+
+def run_simulation(args, F, env, rng):
+    """Run simulation"""
+    env_type = args["env_type"]
+    n_agents = args["n_agents"]
+    intervention = args["intervention"]
+    all_steps = np.array([[i, j] for i in np.arange(-1, 2) for j in np.arange(-1, 2)])
+
+    gammas, etas, rho = F[:,0] * 0.1, F[:,1] * 0.1, F[:,-1]
+    all_solutions, S = initialize_starting_position(env_type, env, n_agents, rho)
+
+    # initialize
+    F_rm = R_rm = None
+    if intervention == True:
+        F_rm, R_rm = initialize_role_models(env_type, env, args["n_roleModels"], rng)
+    W = rng.normal((max(S)-min(S))*0.25, 0.05, (n_agents, 4))
+    A = W @ F.T
+
+    # Track simulation
+    intermediate_solutions = np.zeros_like(all_solutions)
+    intermediate_S = np.zeros_like(S)
+    S_history = np.empty((args["max_iter"], n_agents))
+    S_history[0] = S
+    R = S
+    W_history = np.empty((args["max_iter"], F.shape[1]))
+    W_history[0] = W.mean(axis=0)
+
+    # Repeated processes
+    for t in range(1, args["max_iter"]):
+        Y = softmax((A/A.sum(axis=1)) / args["tau"], axis=1)
+        for agent_i in range(n_agents):
+            # (1) & (2) Individual and social optimization
+            if gammas[agent_i] >= rng.random():
+                probability_distribution = Y[agent_i]
+                new_solutions = all_solutions
+            else:
+                new_solutions, probability_distribution = local_search(env_type, env, S[agent_i], etas[agent_i],
+                                                             all_solutions[agent_i], all_steps,
+                                                             args["tau"], rng)
+            next_solution = draw_step(new_solutions, probability_distribution, rng)
+            intermediate_solutions[agent_i] = next_solution
+            intermediate_S[agent_i] = env.get_fitness(next_solution)
+
+        # (3) Solution implementation
+        S = intermediate_S
+        all_solutions = intermediate_solutions
+        S_history[t] = S
+        R_decay = R_scale[-t:] / R_scale[-t:].sum()
+        R = R_decay @ S_history[:t]
+
+        # (4) Social influence matrix update
+        b = R.min()
+        R_hat = W @ F.T + b
+        grad = loss_gradients(R_hat, F, R)
+        W -= (args["lr"] * grad.T).T
+
+        # Intervention 
+        if intervention is True:
+            if (50 < t <= 50 + args["int_duration"]):
+                grad_rm = run_intervention(args["n_roleModels"], args["sigma"], F, W, F_rm, R_rm, b, rng)
+                W -= args["lr"] * grad_rm
+        
+        W_history[t] = W.mean(axis=0)
+
+    return S_history, W, A, W_history
+
+
+def init_parser():
+    """ Parse arguments for the script"""
+    parser = argparse.ArgumentParser(description='Run Simulation')
+
+    parser.add_argument("--env", '-e', help="Fitness environment", default="MasonWatts")
+    parser.add_argument("--env_type", '-et', help="NK or 2D", default="NK", choices=["NK", "2D"])
+
+    parser.add_argument("--intervention", '-in', help="intervention", default=False, type=bool)
+    parser.add_argument("--int_duration", '-id', help="Duration of intervention", default=10, type=int)
+
+    parser.add_argument("--total_simulations", '-ts', help="Total number of simulations", default=1000, type=int)
+
+    parser.add_argument("--max_iter", '-i', help="Number of Iterations", default=250, type=int)
+    parser.add_argument("--n_agents", '-n', help="Number of agents", default=7, type=int)
+
+    parser.add_argument("--seed", '-s', help="Seed", default=42, type=int)
+
+    args = vars(parser.parse_args())
+
+    # Add aditional parameters
+    args["grid_size"] = 1000
+    args["lambda_scale"]       = 0.9  # Label decay
+    args["beta"]               = 0.5  # Learning rate for social influence matrix A
+    args["tau"]                = 0.01  # Exploitation for social and individual optimization
+    args["sigma"]              = 0.2  # Probability for mentor connections during continuous intervention
+    args["intervention_start"] = 50
+    args["lr"]                 = 0.1  # Learning rate for individual optimization
+    args["n_roleModels"]       = 10  # Number of role models
+
+    all_arg_keys = set(["lr", "tau", "beta", "lambda_scale", "sigma", "intervention_start", "int_duration", "n_roleModels",
+                     "n_agents", "max_iter", "env", "env_type", "seed", "intervention", "total_simulations", "grid_size"])
+    assert len(set(args.keys()).intersection(all_arg_keys)) == len(all_arg_keys), f"Not all necessary arguments are given only: {set(args.keys()).intersection(all_arg_keys)}"
+
+
+    sys.stdout.write(f"Param: value \n")
+    for arg in args.keys():
+        sys.stdout.write(f"{arg}: {args[arg]} \n")
+    return args
+
+def save_results(args, file_title):
+    dir_name = "Sim1_results"
+    if not os.path.exists(dir_name):
+        os.mkdir(dir_name)
+    inter = f"intervention" if args["intervention"] == True else "no_intervention"
+    dir_name += f"/{inter}"
+    if not os.path.exists(dir_name):
+        os.mkdir(dir_name)
+    dir_name += f"/{args['env']}"
+    if not os.path.exists(dir_name):
+        os.mkdir(dir_name)
+    if args["intervention"] == True:
+        file_title += f'_{args["int_duration"]}'
+
+    time = datetime.now().strftime('%Y_%m_%dT%H_%M')
+    dir_name += f"/{time}"
+    os.mkdir(dir_name)
+    return dir_name, file_title
+
+if __name__ == '__main__':
+    """Simulation 1: Individual optimization in a collective with privileg and mentorship"""
+    sys.stdout.write('\n Script is starting\n')
+    args = init_parser()
+
+    # Simulation parameter
+    total_simulations          = args["total_simulations"]
+    n_agents                   = args["n_agents"]
+    max_iterations             = args["max_iter"]
+    R_scale = args["lambda_scale"] ** np.arange(max_iterations - 1, -1, -1)
+
+    # Create environment
+    sys.stdout.write('\nStarting to read environment\n')
+    if args["env_type"] == "NK":
+        N, K, n_landscapes = parse_NK(args['env'])
+        with open(args["env"], 'rb') as f:
+            landscapes = pickle.load(f)
+        for i in range(n_landscapes):
+            keys = landscapes[i].keys()
+            values = np.array(list(landscapes[i].values()))
+            values = values ** 4
+            landscapes[i] = dict(zip(keys, values))
+    elif args["env_type"] == "2D":
+        env = Environment2D(args["env"], args["grid_size"])
+    file_title = env.get_title()
+    sys.stdout.write('\nFinished with reading environment\n')
+
+    # Result saving
+    all_results = np.empty((total_simulations*n_agents, 12))  # Second param: How many vars to track
+    W_results = np.zeros((max_iterations, 4))
+    dir_name, file_title = save_results(args, file_title)
+    np.save(f"{dir_name}/{file_title}", np.array(all_results))
+    with open(f"{dir_name}/parameter.json", "w") as outfile:
+        json.dump(args, outfile)
+
+    # Start simulations
+    base_rng = np.random.default_rng(seed=args["seed"])
+    sys.stdout.write(f"\nIn total {total_simulations} simulations")
+    for _ in range(total_simulations):
+        # Create traits
+        rng = np.random.default_rng(seed=base_rng.integers(0, 100000))
+
+        if args["env_type"] == "NK":
+            env = NK_landscape_loaded(N, K, landscapes[rng.integers(0, n_landscapes)])
+
+        F = rng.random((n_agents, 4))
+        S_history, W, A, W_history = run_simulation(args, F, env, rng)
+
+        run_results = np.concatenate((F,
+                                      W,
+                                      S_history.max(axis=0).reshape(n_agents, 1),
+                                      S_history.mean(axis=0).reshape(n_agents, 1),
+                                      S_history[0].reshape(n_agents, 1),
+                                      A.mean(axis=0).reshape(n_agents, 1),
+                                      ), axis=1)
+
+        all_results[_*n_agents: _*n_agents+n_agents] = run_results
+
+        W_results += W_history
+
+        if (_+1) % 20 == 0:
+            sys.stdout.write(f"\n Simulation: {_}")
+        if _ % 50 == 0:
+            np.save(f"{dir_name}/{file_title}", np.array(all_results))
+    W_results /= total_simulations
+
+    np.save(f"{dir_name}/{file_title}_weights", np.array(W_results))
+    np.save(f"{dir_name}/{file_title}", np.array(all_results))
+    sys.stdout.write(f"\nFinished successfully")
--- environments.py
+++ environments.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.stats import norm, multivariate_normal
 from scipy import interpolate
+from itertools import product
 
 """
 **Environments** 
@@ -11,161 +12,173 @@ from scipy import interpolate
 
 """
 
+class Environment():
 
-def create_env(env_type, grid_size):
-
-    # Fitness landscapes
-    if env_type.lower() == "ackley":
-        fitness_m = ackley(grid_size); env_title = "Ackley"
-    elif env_type.lower() == "masonwatts":
-        fitness_m = mason_watts(grid_size); env_title = "MasonWatts"
-    elif env_type.lower() == "crossintray":
-        fitness_m = cross_in_tray(grid_size); env_title = "CrossInTray"
-    elif env_type.lower() == "shubert":
-        fitness_m = shubert(grid_size); env_title = "Shubert"
-    elif env_type.lower() == "dropwave":
-        fitness_m = drop_wave(grid_size); env_title = "DropWave"
-    elif env_type.lower() == "schaffer":
-        fitness_m = schaffer(grid_size); env_title = "SchafferN4"
-    elif env_type.lower() == "table":
-        fitness_m = holder_table(grid_size); env_title = "HolderTable"
-    else:
-        raise NotImplementedError("Fitness Env not Implemented")
+    def get_fitness(solution):
+        raise NotImplementedError
     
-    fitness_m -= fitness_m.min()
-    fitness_m = fitness_m / fitness_m.max()
+    def get_all_values():
+        raise NotImplementedError
+
+    def get_title():
+        raise NotImplementedError
     
-    return fitness_m, env_title
+class Environment2D(Environment):
+    
+    def __init__(self, env_type, grid_size=1000):
+         # Fitness landscapes
+        if env_type.lower() == "ackley":
+            fitness_m = self.ackley(grid_size); self.env_title = "Ackley"
+        elif env_type.lower() == "masonwatts":
+            fitness_m = self.mason_watts(grid_size); self.env_title = "MasonWatts"
+        elif env_type.lower() == "crossintray":
+            fitness_m = self.cross_in_tray(grid_size); self.env_title = "CrossInTray"
+        elif env_type.lower() == "shubert":
+            fitness_m = self.shubert(grid_size); self.env_title = "Shubert"
+        elif env_type.lower() == "dropwave":
+            fitness_m = self.drop_wave(grid_size); self.env_title = "DropWave"
+        elif env_type.lower() == "schaffer":
+            fitness_m = self.schaffer(grid_size); self.env_title = "SchafferN4"
+        elif env_type.lower() == "table":
+            fitness_m = self.holder_table(grid_size); self.env_title = "HolderTable"
+        else:
+            raise NotImplementedError("Fitness Env not Implemented")
+    
+        fitness_m -= fitness_m.min()
+        self.fitness_m = fitness_m / fitness_m.max()
+        self.grid_size = grid_size
+    
+    def get_fitness(self, solution):
+        return self.fitness_m[solution[0], solution[1]]
+
+    def get_title(self):
+        return self.env_title
+    
+    def get_all_values(self):
+        return self.fitness_m
 
 
-def ackley(grid_size=1000, a=20, b=0.2, c=2 * np.pi):
-    """
-    Create matrix with ackley function between -32.768, 32.768
-    @args: Scales of oscillation/ steepness; default values are the most common used
-    """
-    x = y = np.linspace(-32.768, 32.768, grid_size)
-    xx, yy = np.meshgrid(x, y)
+    def ackley(self, grid_size=1000, a=20, b=0.2, c=2 * np.pi):
+        """
+        Create matrix with ackley function between -32.768, 32.768
+        @args: Scales of oscillation/ steepness; default values are the most common used
+        """
+        x = y = np.linspace(-32.768, 32.768, grid_size)
+        xx, yy = np.meshgrid(x, y)
 
-    # Wikipedia formula
-    sum_1 = 0.5 * (xx ** 2 + yy ** 2)
-    sum_2 = np.cos(c * xx) + np.cos(c * yy)
+        # Wikipedia formula
+        sum_1 = 0.5 * (xx ** 2 + yy ** 2)
+        sum_2 = np.cos(c * xx) + np.cos(c * yy)
 
-    term1 = -a * np.exp(-b * np.sqrt(sum_1))
-    term2 = -np.exp(0.5 * sum_2)
+        term1 = -a * np.exp(-b * np.sqrt(sum_1))
+        term2 = -np.exp(0.5 * sum_2)
 
-    fitness_m = term1 + term2 + a + np.e
+        fitness_m = term1 + term2 + a + np.e
 
-    # Flip and normalize for ascend
-    fitness_m = -fitness_m
-    fitness_m = fitness_m + (-fitness_m.min())
-    fitness_m = fitness_m * (1 / fitness_m.max())
-    return fitness_m
-
-
-def mason_watts(grid_size=1000):
-    """One clear max based on gauß + random noise"""
-    var = 3 * (grid_size / 100)
-    rho = 0.7
-
-    # 1. Generate Random gaussian grid
-    x = y = grid_axes = np.linspace(0, grid_size, grid_size)
-    mu = [np.random.uniform(1, grid_size), np.random.uniform(1, grid_size)]
-    x = norm.pdf(x, loc=mu[0], scale=np.sqrt(var))
-    y = norm.pdf(y, loc=mu[1], scale=np.sqrt(var))
-    fitness_m = np.outer(x, y)
-    fitness_m = fitness_m * (1 / fitness_m.max())  # Note: Not sure if they normalize (taken from 2016 Code)
-
-    # Alternative: Replace by multimodal gaußian with two random means
-    # xx, yy = np.meshgrid(x,y)
-    # xy = np.column_stack([xx.flat, yy.flat])
-    # fitness_m = multivariate_normal.pdf(xy, mean=mu, cov=np.diag([np.sqrt(var), np.sqrt(var)])).reshape(grid_size, grid_size)
-    # fitness_m = fitness_m * (1/ fitness_m.max())
-
-    # 2. Random Perlin noise
-    for omega in range(3, 8):
-        octave = 2 ** omega
-
-        # i) Create smaller grid with uniform values
-        octave_m = np.random.uniform(size=octave ** 2).reshape(octave, octave)
-
-        # ii) Use interpolation to smooth the values of the smaller grid across the neighbours
-        octave_seq = np.linspace(1, grid_size, octave)
-
-        # bicubic interpolation = interpolation of bivariate function based on third degree polynomial
-        # Should be faster, but interpolation does not work smooth: interpBi_m = interpolate.RectBivariateSpline(octave_seq, octave_seq, octave_m, bbox=[min(octave_seq), max(octave_seq), min(octave_seq), max(octave_seq)])
-        interp2d_m = interpolate.interp2d(octave_seq, octave_seq, octave_m, kind="cubic")
-
-        # iii) Scale with persistence parameter
-        octave_m = interp2d_m(grid_axes, grid_axes) * rho ** omega
-
-        fitness_m = fitness_m + octave_m
-
-    # fitness_m = fitness_m * (10 / fitness_m.max())
-    # fitness_m = fitness_m * (100 / fitness_m.max())
-    fitness_m = fitness_m * (1 / fitness_m.max())
-
-    return fitness_m
+        # Flip and normalize for ascend
+        fitness_m = -fitness_m
+        fitness_m = fitness_m + (-fitness_m.min())
+        fitness_m = fitness_m * (1 / fitness_m.max())
+        return fitness_m
 
 
-def cross_in_tray(grid_size=1000):
-    """Formula from: https://www.sfu.ca/~ssurjano/crossit.html
-    and https://www.indusmic.com/post/python-implementation-of-cross-in-tray-function-1"""
-    x = y = np.linspace(-10, 10, grid_size)
-    xx, yy = np.meshgrid(x, y)
+    def mason_watts(self, grid_size=1000):
+        """One clear max based on gauß + random noise"""
+        var = 3 * (grid_size / 100)
+        rho = 0.7
 
-    exp_term = np.abs(100 - np.sqrt(xx ** 2 + yy ** 2) / np.pi)
-    main_part = np.abs(np.sin(xx) * np.sin(yy) * np.exp(exp_term)) + 1
-    fitness_m = -0.0001 * main_part ** 0.1
-    fitness_m = -fitness_m + fitness_m.max()
-    fitness_m = fitness_m * (1 / fitness_m.max())
-    return fitness_m
+        # 1. Generate Random gaussian grid
+        x = y = grid_axes = np.linspace(0, grid_size, grid_size)
+        mu = [np.random.uniform(1, grid_size), np.random.uniform(1, grid_size)]
+        x = norm.pdf(x, loc=mu[0], scale=np.sqrt(var))
+        y = norm.pdf(y, loc=mu[1], scale=np.sqrt(var))
+        fitness_m = np.outer(x, y)
+        fitness_m = fitness_m * (1 / fitness_m.max())  # Note: Not sure if they normalize (taken from 2016 Code)
+
+        # Alternative: Replace by multimodal gaußian with two random means
+        # xx, yy = np.meshgrid(x,y)
+        # xy = np.column_stack([xx.flat, yy.flat])
+        # fitness_m = multivariate_normal.pdf(xy, mean=mu, cov=np.diag([np.sqrt(var), np.sqrt(var)])).reshape(grid_size, grid_size)
+        # fitness_m = fitness_m * (1/ fitness_m.max())
+
+        # 2. Random Perlin noise
+        for omega in range(3, 8):
+            octave = 2 ** omega
+
+            # i) Create smaller grid with uniform values
+            octave_m = np.random.uniform(size=octave ** 2).reshape(octave, octave)
+
+            # ii) Use interpolation to smooth the values of the smaller grid across the neighbours
+            octave_seq = np.linspace(1, grid_size, octave)
+
+            # bicubic interpolation = interpolation of bivariate function based on third degree polynomial
+            # Should be faster, but interpolation does not work smooth: interpBi_m = interpolate.RectBivariateSpline(octave_seq, octave_seq, octave_m, bbox=[min(octave_seq), max(octave_seq), min(octave_seq), max(octave_seq)])
+            interp2d_m = interpolate.interp2d(octave_seq, octave_seq, octave_m, kind="cubic")
+
+            # iii) Scale with persistence parameter
+            octave_m = interp2d_m(grid_axes, grid_axes) * rho ** omega
+
+            fitness_m = fitness_m + octave_m
+
+        fitness_m = fitness_m * (1 / fitness_m.max())
+
+        return fitness_m
+
+    def cross_in_tray(self, grid_size=1000):
+        """Formula from: https://www.sfu.ca/~ssurjano/crossit.html
+        and https://www.indusmic.com/post/python-implementation-of-cross-in-tray-function-1"""
+        x = y = np.linspace(-10, 10, grid_size)
+        xx, yy = np.meshgrid(x, y)
+
+        exp_term = np.abs(100 - np.sqrt(xx ** 2 + yy ** 2) / np.pi)
+        main_part = np.abs(np.sin(xx) * np.sin(yy) * np.exp(exp_term)) + 1
+        fitness_m = -0.0001 * main_part ** 0.1
+        fitness_m = -fitness_m + fitness_m.max()
+        fitness_m = fitness_m * (1 / fitness_m.max())
+        return fitness_m
+
+    def shubert(self, grid_size=1000):
+        """Formula: https://www.sfu.ca/~ssurjano/shubert.html"""
+        x = y = np.linspace(-5.12, 5.12, grid_size)
+        xx, yy = np.meshgrid(x, y)
+        ii = np.arange(1, 6)
+        first_term = np.sum([i * np.cos((i+1) * xx + i) for i in ii], axis=0)
+        second_term = np.sum([i * np.cos((i+1) * yy + i) for i in ii], axis=0)
+        fitness_m = -(first_term * second_term)
+        fitness_m -= fitness_m.min() # + (fitness_m.min())
+        fitness_m = fitness_m * (1/fitness_m.max())
+        return fitness_m
+
+    def drop_wave(self, grid_size=1000):
+        x = y = np.linspace(-5.12, 5.12, grid_size)
+        xx, yy = np.meshgrid(x, y)
+        nominator = 1 + np.cos(12 * np.sqrt(xx ** 2 + yy ** 2))
+        divisor = 0.5 * (xx ** 2 + yy ** 2) + 2
+
+        fitness_m = -(nominator / divisor)
+        # fitness_m -= fitness_m.min() # + (fitness_m.min())
+        fitness_m = fitness_m * (1 / fitness_m.max())
+        return fitness_m
+
+    def schaffer(self, grid_size=1000):
+        x = y = np.linspace(-50, 50, grid_size)
+        xx, yy = np.meshgrid(x, y)
+        nominator = np.cos(np.sin(np.abs(xx**2 - yy**2)))**2 - 0.5
+        divisor = (1 + 0.001*(xx**2 + yy**2))**2
+
+        fitness_m = (nominator / divisor) # +0.5
+        fitness_m = fitness_m * (1 / fitness_m.max())
+        return fitness_m
+
+    def holder_table(self, grid_size=1000):
+        x = y = np.linspace(-10, 10, grid_size)
+        xx, yy = np.meshgrid(x, y)
+        fitness_m = np.abs(np.sin(xx)*np.cos(yy)*np.exp(np.abs(1-(np.sqrt(xx**2 + yy**2)/ np.pi))))
+        fitness_m = fitness_m * (1 / fitness_m.max())
+        return fitness_m
 
 
-def shubert(grid_size=1000):
-    """Formula: https://www.sfu.ca/~ssurjano/shubert.html"""
-    x = y = np.linspace(-5.12, 5.12, grid_size)
-    xx, yy = np.meshgrid(x, y)
-    ii = np.arange(1, 6)
-    first_term = np.sum([i * np.cos((i+1) * xx + i) for i in ii], axis=0)
-    second_term = np.sum([i * np.cos((i+1) * yy + i) for i in ii], axis=0)
-    fitness_m = -(first_term * second_term)
-    fitness_m -= fitness_m.min() # + (fitness_m.min())
-    fitness_m = fitness_m * (1/fitness_m.max())
-    return fitness_m
-
-
-def drop_wave(grid_size=1000):
-    x = y = np.linspace(-5.12, 5.12, grid_size)
-    xx, yy = np.meshgrid(x, y)
-    nominator = 1 + np.cos(12 * np.sqrt(xx ** 2 + yy ** 2))
-    divisor = 0.5 * (xx ** 2 + yy ** 2) + 2
-
-    fitness_m = -(nominator / divisor)
-    # fitness_m -= fitness_m.min() # + (fitness_m.min())
-    fitness_m = fitness_m * (1 / fitness_m.max())
-    return fitness_m
-
-
-def schaffer(grid_size=1000):
-    x = y = np.linspace(-50, 50, grid_size)
-    xx, yy = np.meshgrid(x, y)
-    nominator = np.cos(np.sin(np.abs(xx**2 - yy**2)))**2 - 0.5
-    divisor = (1 + 0.001*(xx**2 + yy**2))**2
-
-    fitness_m = (nominator / divisor) # +0.5
-    fitness_m = fitness_m * (1 / fitness_m.max())
-    return fitness_m
-
-
-def holder_table(grid_size=1000):
-    x = y = np.linspace(-10, 10, grid_size)
-    xx, yy = np.meshgrid(x, y)
-    fitness_m = np.abs(np.sin(xx)*np.cos(yy)*np.exp(np.abs(1-(np.sqrt(xx**2 + yy**2)/ np.pi))))
-    fitness_m = fitness_m * (1 / fitness_m.max())
-    return fitness_m
-
-
-class NK_landscape():
+class NK_landscape(Environment):
 
     def __init__(self, N=10, K=5, seed=None, C=None, dependencies=None):
         """
@@ -238,7 +251,9 @@ class NK_landscape():
         value = self.fitness_dict[key]
         return np.round((value - self.min_value) / (self.max_value - self.min_value), 2)
     
-
+    def get_title(self):
+        return f"N{self.N}K{self.K}"
+    
     
 class NK_landscape_loaded(NK_landscape):
 
@@ -260,4 +275,7 @@ class NK_landscape_loaded(NK_landscape):
     def init_min_max(self):
         self.max_value = max(self.fitness_dict.values())
         self.min_value = min(self.fitness_dict.values())
+    
+    def get_all_values(self):
+        return (self.fitness_dict.values() - self.min_value) / (self.max_value - self.min_value)
 
